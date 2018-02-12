e298179edfad0b5429185702e8e0c0edeab08341
KillerDroid96 <KillerDroid96@users.noreply.github.com>
2018-02-12 10:49:27 -0600

    EET247_LAB_B
    Hello World

---
+++ EET247_LAB_A/cmake-build-debug/CMakeFiles/EET247_LAB_A.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EET247_LAB_A.dir/main.c.o"
+  "EET247_LAB_A.pdb"
+  "EET247_LAB_A.exe"
+  "libEET247_LAB_A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EET247_LAB_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EET247_LAB_A/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    /* This is my First C Program */
+    printf ("Hello, World!\n");
+    getchar();
+    return 0;
+}
