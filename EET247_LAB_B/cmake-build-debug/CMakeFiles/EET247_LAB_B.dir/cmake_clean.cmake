a41afdb3c8a7fd60bd131d57dfe87b981257d142
KillerDroid96 <KillerDroid96@users.noreply.github.com>
2018-02-12 08:02:25 -0600

    EET247_LAB_B

---
+++ EET247_LAB_B/cmake-build-debug/CMakeFiles/EET247_LAB_B.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EET247_LAB_B.dir/main.c.o"
+  "EET247_LAB_B.pdb"
+  "EET247_LAB_B.exe"
+  "libEET247_LAB_B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EET247_LAB_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
